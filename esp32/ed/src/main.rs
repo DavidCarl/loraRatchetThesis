aeb976b42ebd4cdbe06db26cf113d81dfaa3363c
S3j5b0 <asse@itu.dk>
2022-05-02 20:43:43 +0200

    cleaning up

--- esp32/ed/src/main.rs
+++ esp32/ed/src/main.rs
@@ -4,33 +4,26 @@ extern crate alloc;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::time::Duration;
-use std::{env, sync::Arc, thread};
+use std::{ sync::Arc, thread};
 
 use core::convert::TryInto;
 
 use embedded_svc::httpd::*;
-use embedded_svc::ipv4;
-use embedded_svc::ping::Ping;
-
-use embedded_svc::wifi::*;
 
 use esp_idf_svc::netif::*;
 use esp_idf_svc::nvs::*;
-use esp_idf_svc::ping;
 use esp_idf_svc::sysloop::*;
 
-use esp_idf_svc::wifi::*;
 
 use doubleratchet::ed::EDRatchet;
+use wifihandler::wifi;
 
-use rand_core::RngCore;
 use crate::hrng::HRNG;
 mod edhoc;
 mod hrng;
+mod wifihandler;
 const DHR_CONST: u16 = 256;
 
-const SSID: &str =  env!("RUST_ESP32_STD_DEMO_WIFI_SSID");
-const PASS: &str =  env!("RUST_ESP32_STD_DEMO_WIFI_PASS");
 
 fn main() -> Result<()> {
     // initialize wifi stack
@@ -44,9 +37,9 @@ fn main() -> Result<()> {
     #[allow(clippy::redundant_clone)]
     #[allow(unused_mut)]
     let mut wifi = wifi(
-        netif_stack.clone(),
-        sys_loop_stack.clone(),
-        default_nvs.clone(),
+        netif_stack,
+        sys_loop_stack,
+        default_nvs,
     )?;
 
     // open tcp stream
@@ -129,83 +122,3 @@ fn handle_connection(stream: &mut TcpStream) -> Result<(), Error> {
     }
 }
 
-fn wifi(
-    netif_stack: Arc<EspNetifStack>,
-    sys_loop_stack: Arc<EspSysLoopStack>,
-    default_nvs: Arc<EspDefaultNvs>,
-) -> Result<Box<EspWifi>> {
-    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
-
-    println!("Wifi created, about to scan");
-
-    let ap_infos = wifi.scan()?;
-
-    let ours = ap_infos.into_iter().find(|a| a.ssid == SSID);
-
-    let channel = if let Some(ours) = ours {
-        println!(
-            "Found configured access point {} on channel {}",
-            SSID, ours.channel
-        );
-        Some(ours.channel)
-    } else {
-        println!(
-            "Configured access point {} not found during scanning, will go with unknown channel",
-            SSID
-        );
-        None
-    };
-
-    wifi.set_configuration(&Configuration::Mixed(
-        ClientConfiguration {
-            ssid: SSID.into(),
-            password: PASS.into(),
-            channel,
-            ..Default::default()
-        },
-        AccessPointConfiguration {
-            ssid: "aptest".into(),
-            channel: channel.unwrap_or(1),
-            ..Default::default()
-        },
-    ))?;
-
-    println!("Wifi configuration set, about to get status");
-
-    wifi.wait_status_with_timeout(Duration::from_secs(20), |status| !status.is_transitional())
-        .map_err(|e| anyhow::anyhow!("Unexpected Wifi status: {:?}", e))?;
-
-    let status = wifi.get_status();
-
-    if let Status(
-        ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(ip_settings))),
-        ApStatus::Started(ApIpStatus::Done),
-    ) = status
-    {
-        println!("Wifi connected");
-
-        ping(&ip_settings)?;
-    } else {
-        println!("Unexpected Wifi status: {:?}", status);
-    }
-
-    Ok(wifi)
-}
-
-fn ping(ip_settings: &ipv4::ClientSettings) -> Result<()> {
-    println!("About to do some pings for {:?}", ip_settings);
-
-    let ping_summary =
-        ping::EspPing::default().ping(ip_settings.subnet.gateway, &Default::default())?;
-    if ping_summary.transmitted != ping_summary.received {
-        println!(
-            "Pinging gateway {} resulted in timeouts",
-            ip_settings.subnet.gateway
-        );
-        main();
-    }
-
-    println!("Pinging done");
-
-    Ok(())
-}
---
+++ esp32/ed/src/wifihandler.rs
@@ -0,0 +1,100 @@
+use core::convert::TryInto;
+use std::{env, sync::Arc, thread};
+use std::time::Duration;
+
+use embedded_svc::httpd::*;
+use embedded_svc::ipv4;
+use embedded_svc::ping::Ping;
+
+use embedded_svc::wifi::*;
+
+use esp_idf_svc::netif::*;
+use esp_idf_svc::nvs::*;
+use esp_idf_svc::ping;
+use esp_idf_svc::sysloop::*;
+
+use esp_idf_svc::wifi::*;
+
+const SSID: &str =  env!("RUST_ESP32_STD_DEMO_WIFI_SSID");
+const PASS: &str =  env!("RUST_ESP32_STD_DEMO_WIFI_PASS");
+
+
+pub fn wifi(
+    netif_stack: Arc<EspNetifStack>,
+    sys_loop_stack: Arc<EspSysLoopStack>,
+    default_nvs: Arc<EspDefaultNvs>,
+) -> Result<Box<EspWifi>> {
+    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
+
+    println!("Wifi created, about to scan");
+
+    let ap_infos = wifi.scan()?;
+
+    let ours = ap_infos.into_iter().find(|a| a.ssid == SSID);
+
+    let channel = if let Some(ours) = ours {
+        println!(
+            "Found configured access point {} on channel {}",
+            SSID, ours.channel
+        );
+        Some(ours.channel)
+    } else {
+        println!(
+            "Configured access point {} not found during scanning, will go with unknown channel",
+            SSID
+        );
+        None
+    };
+
+    wifi.set_configuration(&Configuration::Mixed(
+        ClientConfiguration {
+            ssid: SSID.into(),
+            password: PASS.into(),
+            channel,
+            ..Default::default()
+        },
+        AccessPointConfiguration {
+            ssid: "aptest".into(),
+            channel: channel.unwrap_or(1),
+            ..Default::default()
+        },
+    ))?;
+
+    println!("Wifi configuration set, about to get status");
+
+    wifi.wait_status_with_timeout(Duration::from_secs(20), |status| !status.is_transitional())
+        .map_err(|e| anyhow::anyhow!("Unexpected Wifi status: {:?}", e))?;
+
+    let status = wifi.get_status();
+
+    if let Status(
+        ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(ip_settings))),
+        ApStatus::Started(ApIpStatus::Done),
+    ) = status
+    {
+        println!("Wifi connected");
+
+        ping(&ip_settings)?;
+    } else {
+        println!("Unexpected Wifi status: {:?}", status);
+    }
+
+    Ok(wifi)
+}
+
+fn ping(ip_settings: &ipv4::ClientSettings) -> Result<()> {
+    println!("About to do some pings for {:?}", ip_settings);
+
+    let ping_summary =
+        ping::EspPing::default().ping(ip_settings.subnet.gateway, &Default::default())?;
+    if ping_summary.transmitted != ping_summary.received {
+        println!(
+            "Pinging gateway {} resulted in timeouts",
+            ip_settings.subnet.gateway
+        );
+    }
+
+    println!("Pinging done");
+
+    Ok(())
+}
